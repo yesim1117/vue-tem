dadfc0dc0333313a3fdce9aa8fbdd3518aade5af
fsws <2523921655@qq.com>
2020-05-14 18:37:11 +0800

    暂缓

---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="g-button">hi</button>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style lang="sass">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em; /* width */
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background-color: var(--button-bg);
+    color: var(--color);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
